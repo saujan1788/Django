4f0ab2a24310699600f24b08a1768e15e7ffaa57
saujan1788 <saujan1788@gmail.com>
2019-05-10 10:29:10 +0100

    Update urls.py

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -17,5 +17,16 @@ urlpatterns = [
         'convert/',
         views.ConvertView.as_view(),
         name='convert',
+    ),
+    path(
+        'valid/',
+        views.Valid_IPv6View.as_view(),
+        name='valid',
+    ),
+
+    path(
+        'expand/',
+        views.ExpandView.as_view(),
+        name='expand',
     )
 ]
